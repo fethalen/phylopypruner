645648fc5fd51a02d3fbf7ae416d7d7f194678c6
Felicia Sandberg <felicia.sandberg@pm.me>
2018-09-24 09:49:35 -0500

    fixed a bug where short sequences were not removed

--- phylopypruner/tree_node.py
+++ phylopypruner/tree_node.py
@@ -184,10 +184,9 @@ class TreeNode(object):
                 # no node to remove could be found
                 break
 
-        # for leaf in self.iter_leaves():
-        #     if not leaf.name:
-        #         leaf.parent.remove_child(leaf)
-        #         leaf.parent = None
+        for leaf in self.iter_leaves():
+            if leaf.name is None:
+                leaf.delete()
 
         return self
 
