8a66417e96b93a07d02b1345985dce9597375da8
Felix Thalén <felix.thalen@gmail.com>
2019-03-28 15:24:17 +0100

    Updated image

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="phylopypruner",
-    version="0.5.1",
+    version="0.5.2",
     author="Felix Thalen",
     author_email="fe1430th-s@student.lu.se",
     license="MIT",
