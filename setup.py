75e275a150a357aff87147c4241763c809250485
Felicia Sandberg <felicia.sandberg@pm.me>
2019-07-12 09:57:41 +0200

    updated README

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="phylopypruner",
-    version="0.8.8",
+    version="0.8.9",
     author="Felix Thalen",
     author_email="fe1430th-s@student.lu.se",
     license="GPL 3",
